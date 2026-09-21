c7d8ba11a19e0a7a64a516bd006cbd5836fcc77c
tacoshopaudio <tacoshopaudio@gmail.com>
2026-09-20 22:58:51 -0400

    Update pebble-js-app.js

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,7 +1,7 @@
 Pebble.addEventListener("showConfiguration",
   function(e) {
     //Load the remote config page
-    Pebble.openURL("http://winterwinter.github.io/DuckHunt/");
+    Pebble.openURL("http://tacoshopaudio.github.io/DuckHunt/");
   }
 );
 
